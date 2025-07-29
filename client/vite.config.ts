205115bc20bddcd242a041ce40a089347a9461e9
shammy14-1 <shammysuyat@gmail.com>
2025-07-29 14:28:24 +0800

    chore: proxy to localhost connection

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss()],
+  server: {
+    proxy:{
+      '/api': {
+        target: 'http://localhost:6000', 
+        changeOrigin:true, 
+        secure:false
+      }
+    },
+
+    host:"192.168.55.100"
+  }
+  
 })
