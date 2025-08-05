7bc95400b22fd06dff8a56dc221d296b3eef4dce
shammy14-1 <shammysuyat@gmail.com>
2025-08-05 21:01:37 +0800

    modifY: logic

--- server/src/controller/message.controller.ts
+++ server/src/controller/message.controller.ts
@@ -2,8 +2,6 @@ import User from "../models/user.model";
 import Message from "../models/message.model";
 import cloudinary from "../lib/cloudinary";
 
-
-
 export const getMessages = async (req: any, res: any) => {
   try {
     const { id } = req.params;
@@ -46,7 +44,7 @@ export const sendMessage = async (req: any, res: any) => {
     await newMessage.save();
     res.status(200).jso(newMessage);
 
-    // this part will use the socket.io 
+    // this part will use the socket.io
   } catch (error) {
     console.log("Error in get messages - message controller errr: ", error);
     res.status(500).json({ message: "Internal Server Error" });
