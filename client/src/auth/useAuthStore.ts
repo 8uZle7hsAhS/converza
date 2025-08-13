76b2f72cff07b9eac2c00ab4bf0a231e6f8ac082
Dannah Joyce Torres <dannahtorres12@gmail.com>
2025-08-13 18:22:44 +0800

    hakdog

--- client/src/auth/useAuthStore.ts
+++ client/src/auth/useAuthStore.ts
@@ -11,7 +11,7 @@ interface AuthState {
   signup: (data: any, navigate: any) => Promise<void>;
   login: () => Promise<void>;
 }
-
+// helloworld hahahh 
 export const useAuthStore = create<AuthState>((set) => ({
   authUser: null,
   isSigningUp: false,
